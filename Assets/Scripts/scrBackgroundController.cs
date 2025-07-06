9e5d911ba522e837a5e89384b6ec9b7afd8386c6
Blortlett <45956871+Blortlett@users.noreply.github.com>
2025-07-07 02:12:45 +1200

    Parallax background

---
+++ Assets/Scripts/scrBackgroundController.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+using static UnityEditor.Experimental.GraphView.GraphView;
+
+public class scrBackgroundController : MonoBehaviour
+{
+    [System.Serializable]
+    public class ParallaxLayer
+    {
+        public Renderer layerRenderer;     // Renderer of the quad using the texture
+        public Vector2 parallaxFactor;     // How much the background moves relative to the camera
+    }
+
+    public ParallaxLayer[] layers;
+    public Transform target;               // Typically your player or camera
+    private Vector3 lastPosition;
+
+    void Start()
+    {
+        if (target == null)
+            target = Camera.main.transform;
+
+        lastPosition = target.position;
+
+        // set transparent material
+        Color color = layers[1].layerRenderer.material.color;
+        color.a = 0.6f; // 60% transparency
+        layers[1].layerRenderer.material.color = color;
+    }
+
+    void LateUpdate()
+    {
+        transform.position = target.transform.position;
+
+        Vector3 deltaMovement = target.position - lastPosition;
+
+        foreach (var layer in layers)
+        {
+            Vector2 offset = layer.layerRenderer.material.mainTextureOffset;
+            offset -= new Vector2(deltaMovement.x, deltaMovement.y) * layer.parallaxFactor;
+            layer.layerRenderer.material.mainTextureOffset = offset;
+        }
+
+        lastPosition = target.position;
+    }
+}
